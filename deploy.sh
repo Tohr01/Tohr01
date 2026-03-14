297b9a82d4f6608e4620cb3fff5ac87f4fe2cf11
Carl Raabe <carl@Mini-von-Carl.fritz.box>
2026-03-14 21:39:20 +0100

    core: adds staging step + verbose stdout

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-
 set -e
 
-cd src
-npm i
-node build
+DEPLOY_DIR="/home/user/static-sites/sites/cr.codes"
+STAGING_DIR_NAME="_staging"
 
-mkdir -p /home/user/static-sites/sites/cr.codes
-cp -r dist/* /home/user/static-sites/sites/cr.codes
+echo "Pulling latest changes from git..."
+git pull
+
+cd src
+echo "Installing dependencies and building the project..."
+npm i && node build
+
+echo "Preparing staging directory..."
+rm -rf "$STAGING_DIR_NAME"
+mkdir -p "$STAGING_DIR_NAME"
+
+echo "Copying built files to staging directory..."
+cp -r dist/. "$STAGING_DIR_NAME"
+
+echo "Removing old files from deployment directory..."
+rm -rf "$DEPLOY_DIR"
+
+echo "Moving new files to deployment directory..."
+mv "$STAGING_DIR_NAME" "$DEPLOY_DIR"
+
+echo "Cleaning up dist directory..."
+rm -rf dist
+
+echo "Deployment complete!"
